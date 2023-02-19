501a38fc0416f14321ed3b8e7ed300f93054a0f6
carlosalexandredev <carlosalexandre5670@gmail.com>
2023-02-19 19:39:34 -0300

    Implementando a edição

--- locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
+++ locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
@@ -2,6 +2,8 @@ package com.algaworks.curso.jpa2.dao;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -9,7 +11,9 @@ import javax.persistence.EntityManager;
 import com.algaworks.curso.jpa2.modelo.Fabricante;
 import com.algaworks.curso.jpa2.service.NegocioException;
 import com.algaworks.curso.jpa2.util.jpa.Transactional;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class FabricanteDAO implements Serializable {
 
 	private static final String BUSCA_TODOS = "FROM Fabricante";
@@ -17,7 +21,7 @@ public class FabricanteDAO implements Serializable {
 	private EntityManager em;
 	
 	public void salvar(Fabricante fabricante) {
-		em.persist(fabricante);
+		em.merge(fabricante);
 	}
 
     public List<Fabricante> buscarTodos() {
@@ -29,5 +33,16 @@ public class FabricanteDAO implements Serializable {
 		fabricanteSelecionado = em.find(Fabricante.class, fabricanteSelecionado.getCodigo());
 		em.remove(fabricanteSelecionado);
 		em.flush();
+	}
+
+	public Fabricante buscarPeloCodigo(Long codigo) {
+		Fabricante fabricante = em.find(Fabricante.class, codigo);
+
+		if(Objects.nonNull(fabricante)) {
+			log.info(String.format("Agenda consultada com sucesso: %s", fabricante.toString()));
+			return fabricante;
+		}else {
+			throw new NoSuchElementException(String.format("A agenda com o ID %s não foi encontrada", codigo));
+		}
 	}
 }
